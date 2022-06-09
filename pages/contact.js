d23a98e248cd71a9d9a50df0a0fdf6e29141c196
Joe Lynn <josephlynn94@gmail.com>
2022-06-08 23:54:26 -0400

    style: fix floating label for name input

--- pages/contact.js
+++ pages/contact.js
@@ -132,12 +132,12 @@ export default function Contact() {
 											id="name"
 											type="text"
 											pattern="\S+.*"
-											class="block p-2.5 px-0 w-full text-sm text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+											class="block p-2.5 w-full text-sm text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 											placeholder=" "
 										/>
 										<label
 											htmlFor="name"
-											class="absolute text-sm text-gray-500 duration-300 transform -translate-y-7 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7"
+											class="absolute text-sm text-gray-500 duration-300 transform -translate-y-7 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7 pl-2.5"
 										>
 											Name
 										</label>
